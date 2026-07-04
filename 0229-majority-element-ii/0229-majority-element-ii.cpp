f9162ee71db56ad63814e60748356dd3a0fee560
Subham Singh <subham.official2901@gmail.com>
2026-07-04 17:40:09 +0200

    Time: 0 ms (100%), Space: 23.7 MB (63.99%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) 
+    {
+        //Moore's Voting Algo Approach ( slightly Modified to address the edge case)
+        int cnt1=0, cnt2=0;
+        int ele1=INT_MIN, ele2=INT_MIN;
+        int n=nums.size();
+        vector<int>result;
+        for(int i=0;i<n;i++)
+        {
+            if(cnt1==0 && ele2!=nums[i])
+            {
+                cnt1=1;
+                ele1=nums[i];
+            }
+            else if(cnt2==0 && ele1!=nums[i])
+            {
+                cnt2=1;
+                ele2=nums[i];
+            }
+            else if(ele1==nums[i])cnt1++;
+            else if(ele2==nums[i])cnt2++;
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1=0;
+        cnt2=0;
+        for(int i =0;i<n;i++)
+        {
+            if(ele1==nums[i])cnt1++;
+            else if(ele2==nums[i])cnt2++;
+        }
+        int req_no=int(n/3)+1;
+        if(cnt1>=req_no)
+        {
+            result.push_back(ele1);
+        }
+        if(cnt2>=req_no)
+        {
+            result.push_back(ele2);
+        }
+
+        return result;
+
+        
+        
+    }
+};
