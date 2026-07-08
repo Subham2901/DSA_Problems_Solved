19dfc6887f66bf450ca1c1d06f4acffbab49669f
Subham Singh <subham.official2901@gmail.com>
2026-07-08 23:16:52 +0200

    Time: 0 ms (100%), Space: 9.3 MB (56.83%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) 
+    {
+        //Using arrays
+        int smap[256]={0};
+        int tmap[256]={0};
+        for(int i=0;i<s.length();i++)
+        {
+            if(smap[s[i]]!=tmap[t[i]])return false;
+            smap[s[i]]=i+1;
+            tmap[t[i]]=i+1;
+        }
+        return true;
+        
+    }
+};
